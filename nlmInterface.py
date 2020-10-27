1a57e20e2a3d573a6af04fe66b374cf11664fdcb
haakooto <hakonolav.torvik@gmail.com>
2020-10-27 09:10:08 +0100

    interactive buttons

---
+++ nlmInterface.py
@@ -0,0 +1,70 @@
+import matplotlib as mpl
+from matplotlib.offsetbox import AnchoredOffsetbox, DrawingArea, TextArea
+from matplotlib.patches import RegularPolygon as RegPoly
+from Hydrogen import Hydrogen
+from numpy import pi
+
+
+class AnchoredInterface(AnchoredOffsetbox):
+    def __init__(self, width, height, xdescent, ydescent):
+        self.area = DrawingArea(width, height, xdescent, ydescent)
+        super().__init__(loc="upper right", pad=0.4, borderpad=0.5,
+                         child=self.area, prop=None, frameon=True)
+
+    def add_text(self, s, xy):
+        mpl.text(s, self.get_extent()[2] + xy[0], self.get_extent()[3] + xy[1])
+
+    def contains(self, mouseevent):
+        """
+        Check is mouse click is in box
+        Returns:
+            a: bool
+            b: dict
+                Is empty dict, can be changed for more info if needed
+        """
+        for c in self.area.get_children():
+            a, b = c.contains(mouseevent)
+            if a:
+                if callable(c):
+                    c()
+                return a, b
+        return False, {}
+
+
+class ClickableArtist(RegPoly):
+    def __init__(self, action, *args, **kwargs):
+        self.action = action
+        super().__init__(*args, **kwargs)
+
+    def __call__(self):
+        return self.action()
+
+
+Hydrogen = Hydrogen()
+Interface = AnchoredInterface(70, 60, 0, 0)
+
+nUp = ClickableArtist(Hydrogen.n_inc, (12, 45), 3, 10, orientation=0)
+lUp = ClickableArtist(Hydrogen.l_inc, (35, 45), 3, 10, orientation=0)
+mUp = ClickableArtist(Hydrogen.m_inc, (58, 45), 3, 10, orientation=0)
+nDown = ClickableArtist(Hydrogen.n_dec, (12, 15), 3, 10, orientation=pi)
+lDown = ClickableArtist(Hydrogen.l_dec, (35, 15), 3, 10, orientation=pi)
+mDown = ClickableArtist(Hydrogen.m_dec, (58, 15), 3, 10, orientation=pi)
+nBox = ClickableArtist(Hydrogen.get_n, (12, 30), 4, 10, orientation=pi/4, fill=False)
+lBox = ClickableArtist(Hydrogen.get_l, (35, 30), 4, 10, orientation=pi/4, fill=False)
+mBox = ClickableArtist(Hydrogen.get_m, (58, 30), 4, 10, orientation=pi/4, fill=False)
+n = mpl.text.Annotation("n", (0.17, 0.44), xycoords=Interface)
+l = mpl.text.Annotation("l", (0.48, 0.43), xycoords=Interface)
+m = mpl.text.Annotation("m", (0.74, 0.44), xycoords=Interface)
+Interface.area.add_artist(nUp)
+Interface.area.add_artist(lUp)
+Interface.area.add_artist(mUp)
+Interface.area.add_artist(nDown)
+Interface.area.add_artist(lDown)
+Interface.area.add_artist(mDown)
+Interface.area.add_artist(nBox)
+Interface.area.add_artist(lBox)
+Interface.area.add_artist(mBox)
+Interface.area.add_artist(n)
+Interface.area.add_artist(l)
+Interface.area.add_artist(m)
+
